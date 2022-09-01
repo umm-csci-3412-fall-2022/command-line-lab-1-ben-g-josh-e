5322cafc382890e02561f306fdfea1d61d166794
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-01 16:36:37 -0500

    Modify for testing purposes without actual distance gained.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -2,9 +2,10 @@
 
 homedir=$(pwd)
 dirname="$1"
-tempdir=$(mktemp -d)
+#tempdir=$(mktemp -d)
+tempdir=/tmp/bob
 
-tar --directory=$tempdir -xzf $dirname #navigate and extract into temporary directory
-grep -Le "DELETE ME!" * | xargs tar -czf "cleaned_$dirname" --directory=$homedir #tar files which do not contain "DELETE ME!" into the start location
-rm -r $tempdir
+tar --directory=$tempdir -xzf "$homedir/$dirname" #navigate and extract into temporary directory
+basename -s .tgz $dirname | xargs grep -Le "DELETE ME!" | xargs tar -czf "cleaned_$dirname" --directory=$homedir #tar files which do not contain "DELETE ME!" into the start location
+#rm -r $tempdir
 
