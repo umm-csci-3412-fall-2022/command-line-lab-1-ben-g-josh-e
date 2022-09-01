3be3af95be81dca49cba29b995f5568f1939df38
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-01 16:05:49 -0500

    Create tgz which does not have the correct contents.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-homedir=pwd
+homedir=$(pwd)
 dirname="$1"
 tempdir=$(mktemp -d)
 
-tar --directory=$tempdir -xzf $dirname
+tar --directory=$tempdir -xzf $dirname #navigate and extract into temporary directory
+grep -Le "DELETE ME!" * | xargs tar -czf "cleaned_$dirname" --directory=$homedir #tar files which do not contain "DELETE ME!" into the start location
+rm -r $tempdir
 
