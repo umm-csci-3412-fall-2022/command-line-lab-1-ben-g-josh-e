01d116f8235f3113a0e20b07234a8862b7a4c864
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-06 15:40:54 -0500

    Update to work within the proper directories

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -2,10 +2,18 @@
 
 homedir=$(pwd)
 dirname="$1"
-#tempdir=$(mktemp -d)
-tempdir=/tmp/bob
+tempdir=$(mktemp -d)
+#tempdir=/tmp/bob
 
 tar --directory=$tempdir -xzf "$homedir/$dirname" #navigate and extract into temporary directory
-basename -s .tgz $dirname | xargs grep -Le "DELETE ME!" | xargs tar -czf "cleaned_$dirname" --directory=$homedir #tar files which do not contain "DELETE ME!" into the start location
+cd "$tempdir"
+#echo $(pwd)
+#echo $(ls)
+basename -s .tgz $dirname | xargs grep -Lre "DELETE ME!" | xargs tar -czf "cleaned_$dirname" #tar files which do not contain "DELETE ME!"
+mv -t "$homedir" "cleaned_$dirname"
+cd "$homedir"
+#echo $(pwd)
+
+#echo "$tempdir"
 #rm -r $tempdir
 
