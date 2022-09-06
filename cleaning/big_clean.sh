3c13ab450bfae7cc1f0ea8e89fcd72572ca0da73
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-06 16:21:21 -0500

    Fix incorrect behavior by switching to explicitly deleting files

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -3,17 +3,14 @@
 homedir=$(pwd)
 dirname="$1"
 tempdir=$(mktemp -d)
-#tempdir=/tmp/bob
 
-tar --directory=$tempdir -xzf "$homedir/$dirname" #navigate and extract into temporary directory
-cd "$tempdir"
-#echo $(pwd)
-#echo $(ls)
-basename -s .tgz $dirname | xargs grep -Lre "DELETE ME!" | xargs tar -czf "cleaned_$dirname" #tar files which do not contain "DELETE ME!"
-mv -t "$homedir" "cleaned_$dirname"
-cd "$homedir"
-#echo $(pwd)
+tar --directory="$tempdir" -xzf "$homedir/$dirname" #navigate and extract into temporary directory
+cd "$tempdir" || exit
 
-#echo "$tempdir"
-#rm -r $tempdir
+
+grep -lre "DELETE ME!" | xargs rm
+tar -czf "$homedir/cleaned_$dirname" -- *
+
+cd "$homedir" || exit
+
 
